017522390919399b9007eea9669db878bc89408c
CR <rajshekhar@protonmail.com>
2018-01-09 08:42:28 -0600

    passport package and google API added for future authentication development

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
 const express = require('express');
+const passport= require('passport');
+const GoogleStrategy =require('passport-google-oauth20').strategy;
 
 //listening requests..btw express and node
 const app = express();
 
-//router handler..express app
+/* //creates new instance..to authenticate user inside app ...passport.use ...is listening particular service here..
+//console.developers.google.com */
+passport.use(new GoogleStrategy());
+
+/* //router handler..express app
 app.get('/',(req,res)=>{
 res.send({hi:'buddy'});
-});
+}); */
 
         
 const PORT = process.env.PORT || 5000;

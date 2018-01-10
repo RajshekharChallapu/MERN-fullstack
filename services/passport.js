3cad5789caeb662c1217e7051719e96889bcaa40
CR <rajshekhar@protonmail.com>
2018-01-10 04:29:59 -0600

    passport callbacks and enabling cookies ..to test  authentication

--- services/passport.js
+++ services/passport.js
@@ -4,21 +4,44 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+
+passport.serializeUser((user, done)=>{
+ done(null, user.id);
+
+});
+passport.deserializeUser((id, done) => {
+   User.findById(id)
+
+.then(user=>{
+    done(null,user);
+});
+
+});
+
+
+
 /* //creates new instance..to authenticate user inside app ...passport.use ...is listening particular service here..passport setup..service config
 //console.developers.google.com */
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
+},
+//callback function
+(accessToken, refreshToken, profile, done) => {
     User
         .findOne({googleId: profile.id})
         .then((existingUser) => {
 
             //tells us ..have a record with given access ID
-            if (existingUser) {} else {
+            if (existingUser) {
+                done(null, existingUser);
+            }
+             else {
                 //if we don't have user ID..it create new record
-                new User({googleId: profile.id}).save();
+                new User({googleId: profile.id})
+                .save()
+                .then(user => done(null,user));
             }
 
         });

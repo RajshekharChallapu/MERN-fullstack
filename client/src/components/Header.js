054df0974fc7ed6d7622193734be355c09f8b1a7
CR <rajshekhar@protonmail.com>
2018-01-11 05:08:51 -0600

    materializecss design (component-navbar)added ... navbar in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 //class base components
-class Header extends Component{
+class Header extends Component {
 
-    render(){
-        return(
-
-            <div>
-            Header
-            </div>
-        )
+    render() {
+        return (
+            <nav>
+           {/*  whenver working with react component we don't use class as property dat as showed in materialze docs..we use `className` */}
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">
+                        Omnv
+                    </a>
+                    <ul className="right">
+                        <li>
+                            <a>Login with Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
     }
 }
 

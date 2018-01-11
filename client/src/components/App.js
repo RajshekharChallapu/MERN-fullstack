6c6db95dbfff9c602fcfd800a5df8847b72a77a4
CR <rajshekhar@protonmail.com>
2018-01-11 04:12:55 -0600

    route configuration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,25 @@
 
 import React from 'react';
+//react router DOM..components
+import {BrowserRouter, Route} from 'react-router-dom';
+
+//dummy componenets
+const Header =() => <h2>Header</h2>;
+const Dashboard =() => <h2>Dashboard</h2>;
+const SurveyNew =() => <h2>SurveyNewr</h2>;
+const Landing =() => <h2>Landing</h2>;
 
 const App =() =>{
     return (
         <div>
-        hi yooo
+       <BrowserRouter>
+       <div>
+       <Route path="/" component={Landing} />
+       </div>
+
+
+      
+       </BrowserRouter>
         </div>
     );
 };

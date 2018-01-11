b91351aa08dd677d263a17bac690074490fb5835
CR <rajshekhar@protonmail.com>
2018-01-11 08:57:07 -0600

    refactoring action creator async and await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import {FETCH_USER} from './types';
 
-//action creator function
-export const fetchUser = () => {
-    return function (dispatch) {
-        axios
-            .get('/api/current_user')
-            .then(res => dispatch({type: FETCH_USER, payload: res}));
-    };
-
+//action creator function...after refactoring
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current_user');
+    dispatch({type: FETCH_USER, payload: res});
 };

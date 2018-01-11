1b6991748e32201d17f96e7b1130ca84a92dcc5f
CR <rajshekhar@protonmail.com>
2018-01-11 04:55:04 -0600

    materialize css ..package added and imported to index.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,28 +1,26 @@
-
 import React from 'react';
 //react router DOM..components
 import {BrowserRouter, Route} from 'react-router-dom';
 
+import Header from './Header';
 //dummy componenets
-const Header =() => <h2>Header</h2>;
-const Dashboard =() => <h2>Dashboard</h2>;
-const SurveyNew =() => <h2>SurveyNewr</h2>;
-const Landing =() => <h2>Landing</h2>;
 
-const App =() =>{
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNewr</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const App = () => {
     return (
         <div>
-       <BrowserRouter>
-       <div>
-       <Header />
-       <Route  exact path="/" component={Landing} />
-       <Route exact path="/surveys" component={Dashboard} />
-       <Route path="/surveys/new" component={SurveyNew} />
-       </div>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/surveys" component={Dashboard}/>
+                    <Route path="/surveys/new" component={SurveyNew}/>
+                </div>
 
-
-      
-       </BrowserRouter>
+            </BrowserRouter>
         </div>
     );
 };
---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+//class base components
+class Header extends Component{
+
+    render(){
+        return(
+
+            <div>
+            Header
+            </div>
+        )
+    }
+}
+
+// can be accessed from other files within the project
+export default Header;

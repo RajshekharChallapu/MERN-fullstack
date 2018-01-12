4d2b2977c8ec6923d857c6f45c502eeaa1edab76
CR <rajshekhar@protonmail.com>
2018-01-12 03:19:53 -0600

    created payment components and added payment method option to view..with successful output test..

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import Payments from './Payments';
 
 //class base components
 class Header extends Component {
@@ -13,9 +14,10 @@ class Header extends Component {
                     <a href="/auth/google">Login with Google</a>
                        </li>;
             default:
-                return <li>
-<a href= "/api/logout">Logout</a>
-                 </li>;
+                return [
+                    <li><Payments/></li>,
+                     <li><a href= "/api/logout">Logout</a></li>
+                ];
         }
     }
 

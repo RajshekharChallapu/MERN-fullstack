2f8bd2b15e59e12805deec8f17c865625ff10afc
CR <rajshekhar@protonmail.com>
2018-01-12 04:29:48 -0600

    billing routes and charges are created ...

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,6 +1,8 @@
 
 import React, {Component} from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
 //class based component
 class Payments extends Component{
@@ -10,7 +12,7 @@ render(){
           name="Omnv"
           description="$5 for 50 email credits"
           amount={500}
-          token={token => console.log(token)}
+          token={token => this.props.handleToken(token)}
           stripeKey={process.env.REACT_APP_STRIPE_KEY}
           >
           <button className="btn">
@@ -21,4 +23,4 @@ render(){
 }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose =require('mongoose');
 const cookieSession = require('cookie-session');
 const passport =require('passport');
+const bodyParser =require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,6 +11,8 @@ mongoose.connect(keys.mongoURI);
 
 //listening requests..btw express and node
 const app = express();
+
+app.use(bodyParser.json());
 
 //middlewares
 app.use(cookieSession({
@@ -20,8 +23,10 @@ keys:[keys.cookieKey]
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 //calling and returns app function from authRoutes file....
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;

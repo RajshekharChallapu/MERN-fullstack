dfb10232b2054b82db40fa117aabeca043bbb952
CR <rajshekhar@protonmail.com>
2018-01-12 04:39:15 -0600

    created charges object in the backend..

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,13 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app =>{
     app.post('/api/stripe',(req,res)=>{
+        stripe.charges.create({
+            amount:500,
+            currency:'usd',
+            description:'$5 for 50 credits',
+            source: req.body.id
+
+        });
 
     });
      

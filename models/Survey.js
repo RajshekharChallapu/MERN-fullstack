36b418738ae7f9282af89ce1caa72fe28d9383b2
CR <rajshekhar@protonmail.com>
2018-01-13 23:55:49 -0600

    setting up subdocs and relationship fields

--- models/Survey.js
+++ models/Survey.js
@@ -9,7 +9,13 @@ body:String,
 subject:String,
 recipients:[RecipientSchema],
 yes:{type: Number, default:0},
-no:{type:Number, default:0}
+no:{type:Number, default:0},
+
+//_user..this type indicates Relationship field btw to another one
+_user:{type: Schema.Types.ObjectId, ref: 'User'},
+    dateSent: Date,
+    lastResponded:Date
+
 
 });
 

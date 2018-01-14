66a74dac06f86b64d9a183953619c7f800847f10
CR <rajshekhar@protonmail.com>
2018-01-14 08:08:20 -0600

    identifying unique users and sendGrid setup for send mass emails

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,24 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits= require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.exports =app =>{
     app.post('/api/surveys',requireLogin, requireCredits, (req, res) =>{
         //making user to have atleast have min credits to take survey
+        //diif properties
+        const {title,subject,body,recipients}= req.body;
+
+        const survey= new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({email:email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
+
+        });
 
     });
 

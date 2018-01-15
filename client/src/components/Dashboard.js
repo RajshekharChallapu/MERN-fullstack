7422fa25dfbe97b10dc3fa4af7f77dbec24b7f17
CR <rajshekhar@protonmail.com>
2018-01-14 23:28:43 -0600

    navigation with link tag and survey new form..

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
 const Dashboard = () =>{
@@ -7,9 +8,10 @@ return (
     <div>
 Dashboard
 <div className="fixed-action-btn">
-<a className="btn-floating btn-large red">
+
+<Link to="/surveys/new" className="btn-floating btn-large red">
 <i className="material-icons">add</i>
-</a>
+</Link>
 </div>
 </div>
 

5311fae6fe4794e5894d6093d09cb383b8e9b34b
CR <rajshekhar@protonmail.com>
2018-01-18 15:32:24 -0600

    styling list with materialcss cards

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -2,20 +2,50 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchSurveys} from '../../actions';
 
-class SurveyList extends Component{
-    componentDidMount(){
-        this.props.fetchSurveys();
+class SurveyList extends Component {
+    componentDidMount() {
+        this
+            .props
+            .fetchSurveys();
     }
-    render(){
-        return(
+
+    renderSurveys() {
+        return this
+            .props
+            .surveys
+            .reverse()
+            .map(survey => {
+                return (
+                    <div className="card  darken-1" key={survey._id}>
+                        <div className="card-content">
+                            <span className="card-title">{survey.title}</span>
+                            <p>
+                                {survey.body}
+                            </p>
+                            <p className="right">
+                                Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+                            </p>
+                        </div>
+                        <div className="card-action">
+                            <a>Yes: {survey.yes}</a>
+                            <a>No: {survey.no}</a>
+                        </div>
+                    </div>
+                );
+            });
+    }
+
+    render() {
+        return (
             <div>
-            SurveyList
+                {this.renderSurveys()}
             </div>
         );
     }
 }
 
-function mapStateToProps(surveys){
+function mapStateToProps({surveys}) {
     return {surveys};
 }
-export default connect(mapStateToProps, {fetchSurveys}) (SurveyList);
+
+export default connect(mapStateToProps, {fetchSurveys})(SurveyList);

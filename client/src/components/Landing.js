cd58be8aa60ae909579f4f592fb52b40bf919287
CR <rajshekhar@protonmail.com>
2018-01-29 11:18:36 -0600

    GIF

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Landing = () =>{
 return (
 <div style={{textAlign:'center'}}>
-<h1>
-YUNO
+<h1 img src = "https://media.giphy.com/media/xUA7aS269qm5PX7fa0/source.gif" >
 </h1>
 Collect feedback from users
 </div>
